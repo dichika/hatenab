147fedff5a4a00541ae2a8f0fabd5a96c8a7a06f
dichika <ichikawa@hcc-jp.com>
2015-01-23 22:56:25 +0900

    bug fix

--- R/hatenab.R
+++ R/hatenab.R
@@ -1,10 +1,28 @@
 #' @export
 getBookmarkCount <- function(url){
   require(httr)
-  u <- paste0("http://api.b.st-hatena.com/entry.counts?", 
-              paste(collapse="&", paste0("url=",url))
-              )
-  res <- GET(u)
-  res <- content(res)
-  return(res)
+  counts <- length(url)
+  if(counts < 50){
+    u <- paste0("http://api.b.st-hatena.com/entry.counts?", 
+                paste(collapse="&", paste0("url=",url))
+    )
+    res <- GET(u)
+    res <- content(res)
+  } else{
+    loops <- trunc(counts/50)+1
+    res <- NULL
+    for(l in seq(loops)){
+      nums <- seq(1+50*(l-1), ifelse(l*50>counts, counts, l*50))
+      url0 <- url[nums]
+      u <- paste0("http://api.b.st-hatena.com/entry.counts?", 
+                  paste(collapse="&", paste0("url=",url0))
+      )
+      res0 <- GET(u)
+      res <- c(res, content(res0))
+      Sys.sleep(0.5)
+    }
+   }
+  res <- unlist(res)
+  res <- data.frame(page=names(res), count=unname(res), stringsAsFactors=FALSE)
+  return(res)    
 }
